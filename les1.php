8720c6f1bf59b80a4d1934b80172097d0cd6eab6
robwigmans <45373471+robwigmans@users.noreply.github.com>
2018-11-26 23:18:00 +0100

    Add files via upload

---
+++ les1.php
@@ -0,0 +1,41 @@
+<?php
+
+// Dit programma print naam
+
+// Initialisatie
+$veld1 = 10;
+$veld2 = 20;
+
+// Hier start je programma
+echo "Rob Wigmans";
+echo "<br>";
+
+echo "Inhoud van de variabele veld1 is: $veld1";
+echo "<br>";
+
+echo "Inhoud van de variabele veld2 is: $veld2";
+
+// Optellen
+echo "<br>";
+$som = $veld1 + $veld2;
+echo "Som is: $som";
+
+
+// Aftrekken
+echo "<br>";
+$uitkomst = $veld1 - $veld2;
+echo "Aftrekken is: $uitkomst";
+
+
+// Gemiddelen
+
+echo "<br>";
+$uitkomst = $som /2;
+echo "Gemiddelde is: $uitkomst";
+
+$adres = "mijn adres is";
+echo "<br>";
+$huidige_datum = date("d/m/y");
+echo $huidige_datum;
+
+?>
